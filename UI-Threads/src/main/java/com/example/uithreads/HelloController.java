972f7ce8b6404fd4ad09f07775d919b8e206171f
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-07-23 15:02:23 -0300

    add threads and runnable example

--- UI-Threads/src/main/java/com/example/uithreads/HelloController.java
+++ UI-Threads/src/main/java/com/example/uithreads/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.uithreads;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
@@ -15,6 +17,8 @@ public class HelloController {
     private Button byeButton;
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     public void initialize() {
         helloButton.setDisable(true);
@@ -29,17 +33,36 @@ public class HelloController {
             System.out.println("Bye, " + nameField.getText());
         }
 
-        try{
-            Thread.sleep(10000);
-        } catch (InterruptedException event) {
-            // we don't care about this
-        }
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    System.out.println("I'm going to sleep on the: " + s);
+                    Thread.sleep(4000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            System.out.println("I'm updating the label on the: " + s);
+                            ourLabel.setText("We did something!");
+                        }
+                    });
+                } catch (InterruptedException event) {
+                    // we don't care about this
+                }
+            }
+        };
+
+        new Thread(task).start();
+
         if(ourCheckBox.isSelected()) {
             nameField.clear();
             helloButton.setDisable(true);
             byeButton.setDisable(true);
         }
     }
+
     @FXML
     public void handleKeyReleased() {
         String text = nameField.getText();
